433aa8a6e17c6310e7a736805cac55941940774c
DanMMX <kabezon_91@hotmail.com>
2013-11-07 09:02:55 -0600

    Update jquery.slidemenu.js
    Added back menu functionality to achieve mobile usage.

--- js/jquery.slidemenu.js
+++ js/jquery.slidemenu.js
@@ -8,11 +8,11 @@
 				var $slidemenu = $(this),
 					data = $slidemenu.data("slidemenu"),
 					$backButton = $slidemenu.find(settings.backButton),
-					$menus = $slidemenu.find("> ul:first-child").find("li"),
+					$menus = $slidemenu.find(" > [data-submenu]"),
 					$submenus, 
 					submenus = "";
 
-				if($backButton.length <= 0 ){
+				if($backButton.length <= 0 && !settings.noBackButtons){
 					$backButton = $("<div />").addClass("backSlideMenuButton").text("<Back");
 
 					$slidemenu.prepend($backButton);
@@ -28,48 +28,74 @@
 					$submenu = $slidemenu.find($elementList.attr("data-submenu"));
 
 					$submenu.hide();
-
+					
 					$elementList.click(function(){
-						if($($submenus).filter(":animated").length == 0){
-							$elementList = $(this);
-							if($elementList.find(settings.backButton).length == 0)
-								if(settings.buttonFunction)
-									buttonFunction($backButton);
+						if(!$(this).hasClass("selected")){
+							$(".selected").removeClass("selected");
+							$(this).addClass("selected");
+							if($slidemenu.filter(":animated").add(".noTransition").length == 0){
+								$elementList = $(this);
+								if($elementList.find(settings.backButton).length == 0)
+									if(settings.buttonFunction)
+										buttonFunction($backButton);
+									else
+										$backButton.slideToggleCss("slow");
 								else
-									$backButton.slideToggle("fast");
-							else
-								if(settings.buttonFunction)
-									buttonFunction($elementList.find(settings.backButton));
-								else
-									$elementList.find(settings.backButton).slideToggle("slow");
+									if(settings.buttonFunction)
+										buttonFunction($elementList.find(settings.backButton));
+									else
+										$elementList.find(settings.backButton).slideToggleCss("slow");
 
+
+								if(settings.menuFunction)
+									menuFunction($menus.not($elementList));
+								else{
+									if(options.hideMenuOnClick){
+										$menus.not($elementList).slideToggleCss("slow");
+									}else{
+										
+									}
+								}
+
+								$submenu = $slidemenu.find($elementList.attr("data-submenu"));							
+								$submenus.not($submenu).slideUpCss("slow");
+								if(settings.submenuFunction)
+									submenuFunction($submenu);
+								else
+									$submenu.slideDownCss("slow");
+							}
+						}else{
+							$(".selected").removeClass("selected");
 							$submenu = $slidemenu.find($elementList.attr("data-submenu"));
-							if(settings.submenuFunction)
-								submenuFunction($submenu);
-							else
-								$submenu.slideToggle("slow");
-
-							if(settings.menuFunction)
-								menuFunction($menus.not($elementList));
-							else
-								$menus.not($elementList).slideToggle("slow");
+							$submenu.slideUpCss("slow");
 						}
 					});
+					$(document).scrollTop(0);
+					$menus.scrollTop(0);
 				});
 	
 				$submenus = $(submenus);
+				if(settings.autoHideBackButtons)
+					$backButton.hide();
 
-				$backButton.hide().click(function(){
-					$menus.filter(":hidden").slideToggle("slow");
+				$backButton.click(function(){
+					$menus.filter(":hidden").slideToggleCss("slow");
 
-					$submenus.not(":hidden").slideToggle("slow");
-					$backButton.not(":hidden").slideToggle("slow");
+					if(settings.submenuFunction)
+						buttonFunction($submenus.not(":hidden"));
+					else
+						$submenus.not(":hidden").slideToggleCss("slow");
+					
+					if(settings.buttonFunction)
+						buttonFunction($(this));
+					else
+						$backButton.not(":hidden").slideToggleCss("slow");
 				});
 
 				var hashVal = window.location.hash.split("#")[1];
-				if(hashVal !== "undefined"){
+				if(hashVal){
 					$slidemenu.find("[data-submenu=#"+hashVal+"]").click();
-
+					
 					$(document).scrollTop(0);
 					$(".paper.menu.two-thirds.column").scrollTop(0);
 				}
@@ -98,7 +124,9 @@
 	};
 
 	$.fn.slidemenu.defaults = {
-		"backButton" : ".backSlideMenuButton"
+		"backButton" : ".backSlideMenuButton",
+		"noBackButtons" : false,
+		"hideMenuOnClick" : true
 	}
 	
 })(jQuery);
